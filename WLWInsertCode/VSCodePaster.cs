9bbb4c9ba605cc7ed682979971dd023e6b27959e
Julian M Bucknall <julianb@boyet.com>
2017-01-04 18:29:49 -0700

    Initial Code Commit
    Reverted to supporting Windows Live Writer with this project, since have
    now created an Open Live Writer project for the same functionality.

---
+++ WLWInsertCode/VSCodePaster.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsLive.Writer.Api;
+using RtfToHtml;
+
+namespace WLWInsertCode {
+
+  [WriterPlugin("F85C3533-BC93-49e8-8273-4FA33D21EDCC", "PasteCodeFromVS")]
+  [InsertableContentSource("Paste Code From VS")]
+  public class VSCodePaster : ContentSource {
+
+    public override DialogResult CreateContent(IWin32Window dialogOwner, ref string content) {
+      DialogResult result = DialogResult.OK;
+
+      if (Clipboard.ContainsText(TextDataFormat.Rtf)) {
+        string s = Clipboard.GetText(TextDataFormat.Rtf);
+        string html = RtfParser.ParseRtf(s);
+        content = String.Format("<div class=\"jmbcodeblock\"><pre>{0}{1}</pre></div>{0}", Environment.NewLine, html);
+      }
+      else {
+        content = "Clipboard is empty of RTF text";
+      }
+      return result;
+    }
+  }
+}
